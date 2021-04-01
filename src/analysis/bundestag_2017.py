9cec433583c11ffd1f0680222569bd6b51088ad1
JakobWegmann <37152510+JakobWegmann@users.noreply.github.com>
2021-04-01 12:37:52 +0200

    Cleaned up.

--- src/analysis/bundestag_2017.py
+++ src/analysis/bundestag_2017.py
@@ -22,12 +22,6 @@ from functions_law import allocation_seats_after2013
 from functions_law import eligible_parties
 from functions_law import last_allocation_seats
 
-# from functions_law import sainte_lague
-
-# from functions_law import size_bundestag
-
-# from functions_law import redistribution_bundestag_seats
-
 if user == "Jakob":
     path = "C:/Users/jakob/sciebo/Bonn/6th_semester/election_calculator"
 elif user == "Dominik":
@@ -38,15 +32,9 @@ else:
 # * STEP 1: Calculate initial number of seats for each state.
 population = pd.read_json(f"{path}/bld/data/population_data.json")
 population.set_index(["Bundesland"], inplace=True)
-# ! Only 25% sure what I´m doing
 population = pd.to_numeric(population["Deutsche"])
 min_seats_bundestag = 598
 initial_seats_by_state = allocation_seats_after2013(population, min_seats_bundestag)
-
-# prelim_divisor = population.sum() / 598
-# initial_seats_by_state, final_divisor = sainte_lague(
-#     prelim_divisor, population, int(598)
-# )
 
 # * Step 2: Calculate the number of Direktmandate.
 # * Load the data we need. (Left as raw as possible.)
@@ -207,6 +195,10 @@ possible_coalition["margin"] = possible_coalition["sum_seats"] - necc_votes_maj
 possible_coalition["possible coalition"] = possible_coalition["margin"].apply(
     lambda x: "possible" if x >= 0 else "not possible"
 )
+
+# * Identifying marginal results with large implications
+# Idea: Change Erststimmen marginally, check the effect
+
 
 # offene Baustellen:
 # TODO Relative Pfade (pytask? oder zu nervig?)
